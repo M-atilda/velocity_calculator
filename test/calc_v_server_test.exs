84d8621cf08565a956bf299620854130ecaec7ab
M-atilda <lmatilda.gp100@gmail.com>
2018-01-17 16:52:19 +0900

    fix the size bug

--- test/calc_v_server_test.exs
+++ test/calc_v_server_test.exs
@@ -1,14 +1,15 @@
 defmodule CalcVServerTest do
   use ExUnit.Case
   doctest CalcVServer
+  @tag timeout: 600000
 
   test "calculate next step velocity" do
-    v_field = List.duplicate(List.duplicate([0|List.duplicate(1, 7)], 8), 8)
-    p_field = List.duplicate(List.duplicate(List.duplicate(1, 8), 8), 8)
-    bc_field = for k <- 0..7 do
-      for j <- 0..7 do
-        for i <- 0..7 do
-          if 4==i && 4==j && 4==k do
+    v_field = List.duplicate(List.duplicate([0|List.duplicate(1, 100)], 101), 101)
+    p_field = List.duplicate(List.duplicate([0|List.duplicate(1, 100)], 101), 101)
+    bc_field = for k <- 0..100 do
+      for j <- 0..100 do
+        for i <- 0..100 do
+          if 24==i || 24==j || 24==k do
             0
           else
             nil
@@ -16,9 +17,9 @@ defmodule CalcVServerTest do
         end end end
     CalcVServer.genCalcServer(:u)
     result = CalcVServer.calcVel :u, {v_field, v_field, v_field}, p_field, bc_field,
-      %{:x_size => 8,
-        :y_size => 8,
-        :z_size => 8,
+      %{:x_size => 101,
+        :y_size => 101,
+        :z_size => 101,
         :dx => 0.1,
         :dy => 0.1,
         :dz => 0.1,
